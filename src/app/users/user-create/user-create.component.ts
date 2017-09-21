2e893d231fa8782f0bab42dcc08dfb684adafb6f
Derek Campanile <me@dpcamp.com>
2017-09-21 11:16:18 -0700

    Production Fixes

--- src/app/users/user-create/user-create.component.ts
+++ src/app/users/user-create/user-create.component.ts
@@ -1,36 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { User } from '../../shared/models/user';
-import { UserService } from '../../shared/services/user.service';
 
 @Component({
   templateUrl: 'user-create.component.html'
 })
-export class UserCreateComponent implements OnInit {
-  //user: User = { id: '', titlegivenName: '', sn: '', sAMAccountName: '', mail: '', };
-  successMessage: string = '';
-  errorMessage: string = '';
+export class UserCreateComponent {
 
-  constructor(private service: UserService, private router: Router) { }
 
-  ngOnInit() {
-  }
 
-  /**
-   * Create a user
-   */
-  createUser() {
-    this.successMessage = '';
-    this.errorMessage   = '';
-
-/*     this.service.createUser(this.user)
-      .subscribe(user => {
-        this.successMessage = 'User was created!';
-        console.log('user was created');
-
-        // navigate back to the users page
-        this.router.navigate(['/users']);
-      }) */
-  }
 
 }
