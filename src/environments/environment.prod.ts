ac0144a959cb56e94ba9ee31c117951a8278bf10
VMS System Administrator <entadmin@GRF-LW.INTERNAL>
2018-02-09 15:19:58 -0800

    updated prod server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  usersUrl: 'http://172.20.20.126:3000/api/users',
-  computersUrl: 'http://172.20.20.126:3000/api/computers',
-  phonesUrl: 'http://172.20.20.126:3000/api/phones',
-  reportUrl: 'http://172.20.20.126:3000/api/reports'
+  usersUrl: 'http://172.16.10.69:3000/api/users',
+  computersUrl: 'http://172.16.10.69:3000/api/computers',
+  phonesUrl: 'http://172.16.10.69:3000/api/phones',
+  reportUrl: 'http://172.16.10.69:3000/api/reports'
 };
