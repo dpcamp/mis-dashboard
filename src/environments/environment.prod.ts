099873650354f081b86a8466a5a8d886970230ae
Derek Campanile <derek@dpc-designs.com>
2018-08-21 08:52:57 -0700

    Production Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   createUsersUrl: 'http://172.16.10.69:8889/auth/create',
   computersUrl: 'http://172.16.10.69:3000/api/computers',
   phonesUrl: 'http://172.16.10.69:3000/api/phones',
-  reportUrl: 'http://172.16.10.69:3000/api/reports'
+  reportUrl: 'http://172.16.10.69:3000/api/reports',
+  authUrl: 'http://GRF-MISDEV:8888/'
 };
