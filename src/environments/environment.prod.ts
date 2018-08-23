4115f5cce7be77d9d5600d44c54dc1f9053313c6
Derek Campanile <derek@dpc-designs.com>
2018-08-23 16:02:29 -0700

    prod env update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
   computersUrl: 'http://172.16.10.69:3000/api/computers',
   phonesUrl: 'http://172.16.10.69:3000/api/phones',
   reportUrl: 'http://172.16.10.69:3000/api/reports',
-  authUrl: 'http://GRF-MISDEV:8888/'
+  authUrl: 'http://GRF-MISDEV:8888/',
+  validationUrl: 'http://GRF-MISDEV:8889/validate'
 };
